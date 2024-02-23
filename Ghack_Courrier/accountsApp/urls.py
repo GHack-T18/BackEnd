08e656a611ce9ff41b9c80fa8d6d6faf99ed2eb4
Nada <ln_kouadri@esi.dz>
2024-02-23 13:33:50 +0000

    Project initiation

--- Ghack_Courrier/accountsApp/urls.py
+++ Ghack_Courrier/accountsApp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('redirect/',views.redirect_loggedin_user,name='login'),
+    path('addUser1/',views.Add_User1,name='addUser1'),
+    path('addUser2/',views.Add_User2,name='addUser2'),
+    path('addUser3/',views.Add_User3,name='addUser3'),
+    path('userinfo/',views.current_user,name='user_info'),
 
 ]
